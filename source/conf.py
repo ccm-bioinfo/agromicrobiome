519d5021899ff8a52573585e75e8370058056407
aapashkov <aapashkov@hotmail.com>
2025-06-21 11:19:15 -0600

    feat: setup sphinx

---
+++ source/conf.py
@@ -0,0 +1,27 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'Agromicrobiome Manual'
+copyright = '2025, César Aguilar, Fernando Fontove-Herrera, David A. García-Estrada, Haydeé Contreras-Peruyero, Anton Pashkov, Shaday Guerrero-Flores, Nelly Sélem-Mojica'
+author = 'César Aguilar, Fernando Fontove-Herrera, David A. García-Estrada, Haydeé Contreras-Peruyero, Anton Pashkov, Shaday Guerrero-Flores, Nelly Sélem-Mojica'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
---
+++ source/index.rst
@@ -0,0 +1,20 @@
+.. Agromicrobiome Manual documentation master file, created by
+   sphinx-quickstart on Sat Jun 21 11:07:37 2025.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to Agromicrobiome Manual's documentation!
+=================================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
